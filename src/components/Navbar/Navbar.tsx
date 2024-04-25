ba48de84fa4f5763acfa8e9b7694ac7f70c571d7
gramizor <granin17@yandex.ru>
2024-04-25 04:44:05 +0300

    navbar edition

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,28 +4,54 @@ import { useTheme } from "../../hooks/useTheme";
 import { Button } from "../UI";
 import styles from "./styles.module.css";
 import { logoutUser } from "../../core/auth/layer";
-import { NavLink } from "react-router-dom";
 import { RoutesEnum } from "../../app/routes";
+import { useDisclosure } from "@mantine/hooks";
+import { Burger } from "@mantine/core";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const { t } = useTranslation();
   const { toggleThemeMode } = useTheme();
+  const [opened, { toggle }] = useDisclosure();
+
+  const toggleMenu = () => {
+    toggle();
+  };
+
   return (
     <nav className={styles.nav}>
-      <p className={styles.title}>The Brawlers2.0</p>
-      <Button onClick={toggleLanguage} label={t("common.ChangeLanguage")} />
-      <Button
-        onClick={toggleThemeMode}
-        color="tertiary"
-        label={t("common.ChangeTheme")}
-      />
-      <Button onClick={logoutUser} label={t("common.Logout")} color="error" />
-      <NavLink to={RoutesEnum.Stack}>
-        <Button label={t("common.StackOverflow")} />
-      </NavLink>
-      <NavLink to={RoutesEnum.Home}>
-        <Button label={t("common.Home")} />
-      </NavLink>
+      <div className={styles.leftSection}>
+        <p className={styles.title}>The Brawlers2.0</p>
+        <Link to={RoutesEnum.Home}>
+          <Button label={t("common.Home")} />
+        </Link>
+        <Link to={RoutesEnum.Stack}>
+          <Button label={t("common.StackOverflow")} />
+        </Link>
+      </div>
+      <div className={styles.menu}>
+        <Burger
+          opened={opened}
+          onClick={toggleMenu}
+          aria-label="Toggle navigation"
+        />
+        <div className={`${styles.menuItems} ${opened ? styles.show : ''}`}>
+          <Button
+            onClick={toggleLanguage}
+            label={t("common.ChangeLanguage")}
+          />
+          <Button
+            onClick={toggleThemeMode}
+            color="tertiary"
+            label={t("common.ChangeTheme")}
+          />
+          <Button
+            onClick={logoutUser}
+            label={t("common.Logout")}
+            color="error"
+          />
+        </div>
+      </div>
     </nav>
   );
 };
