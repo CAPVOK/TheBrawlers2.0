6c4486c32b6c58353c4bd3b89b6ce00b8ff21f79
kabanets Vladimir <capvok@yandex.ru>
2024-04-25 13:16:34 +0300

    login page

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -2,34 +2,88 @@ import { Link, useNavigate } from "react-router-dom";
 import { RoutesEnum } from "../../app/routes/routes";
 import styles from "./styles.module.css";
 import { loginUser } from "../../core/auth/layer";
-import { Button, IButtonProps } from "../../components/UI";
 import { useTranslation } from "react-i18next";
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
+import {
+  TextInput,
+  PasswordInput,
+  Anchor,
+  Paper,
+  Title,
+  Text,
+  Container,
+  Button,
+} from "@mantine/core";
+import { IUserLoginData } from "../../core/auth/types";
 
 export const LoginPage = () => {
   const navigate = useNavigate();
   const { t } = useTranslation();
   const [isBtnLoading, setIsBtnLoading] = useState(false);
+  const [formData, setFormData] = useState<IUserLoginData>({
+    email: "",
+    password: "",
+  });
+
+  const isButtonDisabled = !formData.email || !formData.password;
 
   const handleLoginClick = async () => {
+    if (isButtonDisabled) return;
     setIsBtnLoading(true);
-    loginUser({ email: "vova", password: "vova" }).then(() =>
-      navigate(RoutesEnum.Home)
-    );
+    loginUser(formData)
+      .then(() => navigate(RoutesEnum.Home))
+      .finally(() => setIsBtnLoading(false));
   };
 
-  const loginButtonProps: IButtonProps = {
-    label: t("common.Login"),
-    isLoading: isBtnLoading,
-    onClick: handleLoginClick,
+  const handleDataChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const name = e.target.name;
+    if (!(name in formData)) return;
+    setFormData({ ...formData, [name]: e.target.value });
   };
 
   return (
     <div className={styles.page}>
-      <h1>LoginPage h1</h1>
-      <p>LoginPage p</p>
-      <Button {...loginButtonProps} />
-      <Link to={RoutesEnum.Register}>To register</Link>
+      <Container size={420} my={40}>
+        <Title ta="center" className={styles.title}>
+          {t("common.welcomeBack")}
+        </Title>
+        <Text c="dimmed" size="sm" ta="center" mt={5}>
+          {t("common.noAccountYet")}{" "}
+          <Anchor to={RoutesEnum.Register} size="sm" component={Link}>
+            {t("common.createAccount")}
+          </Anchor>
+        </Text>
+
+        <Paper withBorder shadow="md" p={30} mt={30} radius="md">
+          <TextInput
+            label="Email"
+            name="email"
+            onChange={handleDataChange}
+            placeholder="you@mantine.dev"
+            value={formData.email}
+            required
+          />
+          <PasswordInput
+            label={t("common.Password")}
+            placeholder={t("common.YourPassword")}
+            name="password"
+            value={formData.password}
+            onChange={handleDataChange}
+            required
+            mt="md"
+          />
+          <Button
+            fullWidth
+            mt="xl"
+            onClick={handleLoginClick}
+            loading={isBtnLoading}
+            disabled={isButtonDisabled}
+            color={"var(--clr-primary)"}
+          >
+            {t("common.Login")}
+          </Button>
+        </Paper>
+      </Container>
     </div>
   );
 };
--- src/pages/RegisterPage/RegisterPage.tsx
+++ src/pages/RegisterPage/RegisterPage.tsx
@@ -2,21 +2,106 @@ import { Link, useNavigate } from "react-router-dom";
 import { RoutesEnum } from "../../app/routes/routes";
 import styles from "./styles.module.css";
 import { registerUser } from "../../core/auth/layer";
+import {
+  TextInput,
+  PasswordInput,
+  Anchor,
+  Paper,
+  Title,
+  Text,
+  Container,
+  Button,
+} from "@mantine/core";
+
+import { ChangeEvent, useState } from "react";
+import { useTranslation } from "react-i18next";
+import { IUserRegisterData } from "../../core/auth/types";
+
+interface IUserRegisterFromData extends IUserRegisterData {
+  password2: string;
+}
 
 export const RegisterPage = () => {
   const navigate = useNavigate();
+  const { t } = useTranslation();
+  const [isBtnLoading, setIsBtnLoading] = useState(false);
+  const [formData, setFormData] = useState<IUserRegisterFromData>({
+    email: "",
+    password: "",
+    password2: "",
+  });
+
+  const isButtonDisabled =
+    !formData.email ||
+    !formData.password ||
+    !formData.password2 ||
+    formData.password !== formData.password2;
 
   const handleRegisterClick = async () => {
-    registerUser({ email: "vova", password: "vova" }).then(() =>
-      navigate(RoutesEnum.Home)
-    );
+    if (isButtonDisabled) return;
+    registerUser({ email: formData.email, password: formData.password })
+      .then(() => navigate(RoutesEnum.Home))
+      .finally(() => setIsBtnLoading(false));
+  };
+
+  const handleDataChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const name = e.target.name;
+    if (!(name in formData)) return;
+    setFormData({ ...formData, [name]: e.target.value });
   };
 
   return (
     <div className={styles.page}>
-      <h1>RegisterPage</h1>
-      <button onClick={handleRegisterClick}>Зарегаться</button>
-      <Link to={RoutesEnum.Login}>To login</Link>
+      <Container size={420} my={40}>
+        <Title ta="center" className={styles.title}>
+          {t("common.welcome")}
+        </Title>
+        <Text c="dimmed" size="sm" ta="center" mt={5}>
+          {t("common.haveAccountAlready")}{" "}
+          <Anchor to={RoutesEnum.Login} size="sm" component={Link}>
+            {t("common.Login")}
+          </Anchor>
+        </Text>
+
+        <Paper withBorder shadow="md" p={30} mt={30} radius="md">
+          <TextInput
+            label="Email"
+            name="email"
+            onChange={handleDataChange}
+            placeholder="you@mantine.dev"
+            value={formData.email}
+            required
+          />
+          <PasswordInput
+            label={t("common.Password")}
+            placeholder={t("common.YourPassword")}
+            name="password"
+            value={formData.password}
+            onChange={handleDataChange}
+            required
+            mt="md"
+          />
+          <PasswordInput
+            label={t("common.ConfirmThePassword")}
+            placeholder={t("common.YourPassword")}
+            name="password2"
+            value={formData.password2}
+            onChange={handleDataChange}
+            required
+            mt="md"
+          />
+
+          <Button
+            fullWidth
+            mt="xl"
+            onClick={handleRegisterClick}
+            loading={isBtnLoading}
+            disabled={isButtonDisabled}
+          >
+            {t("common.Register")}
+          </Button>
+        </Paper>
+      </Container>
     </div>
   );
 };
