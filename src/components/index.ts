71b1597d321ef0f1cc8b7da95d2563a13c8e401d
kabanets Vladimir <capvok@yandex.ru>
2024-04-27 19:29:05 +0300

    cases bar fix

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,6 @@ import { PrivateRoute } from "./PrivateRoute/PrivateRoute";
 import TaskItem from "./TaskItem/TaskItem";
 import Navbar from "./Navbar/Navbar";
 import CaseItem from "./CaseItem/CaseItem";
+import SkeletonItem from "./SkeletonItem/SkeletonItem";
 
-export { PrivateRoute, TaskItem, Navbar, CaseItem };
+export { PrivateRoute, TaskItem, Navbar, CaseItem, SkeletonItem };
--- src/modules/CasesBar/CasesBar.tsx
+++ src/modules/CasesBar/CasesBar.tsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from "react";
-import { CaseItem } from "../../components";
+import { useEffect, useRef, useState } from "react";
+import { CaseItem, SkeletonItem } from "../../components";
 import { useCases } from "../../store/casesSlice";
 import styles from "./styles.module.css";
 import { useTasks } from "../../store/tasksSlice";
@@ -14,21 +14,36 @@ function CasesBar() {
   const { t } = useTranslation();
 
   const [cases, setCases] = useState<ICase[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
   const [isAddCaseLoadin, setCaseLoading] = useState(false);
 
   const { activeTask, draftTasks, activeTasks } = useTasks();
   const { activeCase, changeActiveCase, closeCase } = useCases();
 
-  async function getCasesByClusterDebounce(cluster: ICluster["id"]) {
+  const prevTask = useRef<number | null>(null);
+
+  /* async function getCasesByClusterDebounce(cluster: ICluster["id"]) {
     const loadingTimer = setTimeout(() => setIsLoading(true), 1000);
     await getCasesByCluster(cluster).then((cases) => {
       if (cases) {
         setCases(cases);
+      } else {
+        setCases([]);
       }
     });
-
     clearTimeout(loadingTimer);
+    setIsLoading(false);
+  } */
+
+  async function getCasesByClusterDebounce(cluster: ICluster["id"]) {
+    setIsLoading(true);
+    await getCasesByCluster(cluster).then((cases) => {
+      if (cases) {
+        setCases(cases);
+      } else {
+        setCases([]);
+      }
+    });
     setIsLoading(false);
   }
 
@@ -37,28 +52,29 @@ function CasesBar() {
 
   useEffect(() => {
     if (taskData) {
-      getCasesByClusterDebounce(taskData.cluster.id);
+      if (
+        !prevTask.current ||
+        prevTask.current === -1 ||
+        prevTask.current !== taskData.cluster.id
+      ) {
+        getCasesByClusterDebounce(taskData.cluster.id).finally(() => {
+          prevTask.current = taskData.cluster.id;
+        });
+      } else {
+        getCasesByCluster(taskData.cluster.id)
+          .then((cases) => {
+            if (cases) {
+              setCases(cases);
+            } else {
+              setCases([]);
+            }
+          })
+          .finally(() => {
+            prevTask.current = taskData.cluster.id;
+          });
+      }
     }
   }, [activeTask, taskData]);
-
-  /* useEffect(() => {
-    if (!taskData) {
-      return;
-    }
-    if (activeCase === -1) {
-      setCases([]);
-    }
-    const intervalId = setInterval(() => {
-      getCasesByCluster(taskData.cluster.id).then((cases) => {
-        if (cases) {
-          setCases(cases);
-        }
-      });
-    }, 5000);
-    return () => {
-      clearInterval(intervalId);
-    };
-  }, [activeCase, activeTask, taskData]); */
 
   const handleCaseClick = (id: number) => {
     if (activeCase === id) {
@@ -75,21 +91,21 @@ function CasesBar() {
     }
   };
 
-  if (activeTask === -1 || !cases) {
-    return <div className={styles.nodata}>{t("pages.noCases")}</div>;
-  }
+  const isContent = !!cases && !!cases.length;
 
   return (
     <div className={styles.sidebar}>
       <h2 className={styles.title}>{t("components.case.Solutions")}</h2>
       <div className={styles["scroll-block"]}>
-        {isLoading ? (
-          <div className={styles.loading}>
-            <p>{t("common.Loading")}</p>
-          </div>
-        ) : (
-          <div className={styles.content}>
-            {cases.map((item) => (
+        <div className={styles.content}>
+          {isLoading ? (
+            <>
+              <SkeletonItem />
+              <SkeletonItem />
+              <SkeletonItem />
+            </>
+          ) : isContent ? (
+            cases.map((item) => (
               <CaseItem
                 key={item.id}
                 title={item.title}
@@ -103,9 +119,11 @@ function CasesBar() {
                   taskData.case?.id === activeCase
                 }
               />
-            ))}
-          </div>
-        )}
+            ))
+          ) : (
+            <p>{t("pages.noCases")}</p>
+          )}
+        </div>
       </div>
     </div>
   );
