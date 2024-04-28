085c4f3d11a0a68c299ab00b497f8c63a3a31956
kabanets Vladimir <capvok@yandex.ru>
2024-04-28 06:23:18 +0300

    slider

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -5,17 +5,59 @@ import { Navbar } from "../../components";
 import CasesBar from "../../modules/CasesBar/CasesBar";
 import { MainPageContent } from "../../modules/MainPageContent/MainPageContent";
 import { useTasks } from "../../store/tasksSlice";
+import { useEffect } from "react";
 
-export const MainPage = () => {
+const MainPage = () => {
   const { activeTask } = useTasks();
+
+  useEffect(() => {
+    const resizableBlock = document.getElementById("resizableBlock");
+    let isResizing = false;
+    let prevX = 0;
+    if (!resizableBlock) return;
+
+    const handleMouseMove = (event: MouseEvent) => {
+      if (isResizing) {
+        const deltaX = event.clientX - prevX;
+        const newWidth = resizableBlock.offsetWidth - deltaX;
+        resizableBlock.style.width = `${newWidth}px`;
+        prevX = event.clientX;
+      }
+    };
+    const handleMouseDown = (event: MouseEvent) => {
+      isResizing = true;
+      prevX = event.clientX;
+      document.addEventListener("mousemove", handleMouseMove);
+      document.addEventListener("mouseup", handleMouseUp);
+    };
+
+    const handleMouseUp = () => {
+      isResizing = false;
+      document.removeEventListener("mousemove", handleMouseMove);
+      document.removeEventListener("mouseup", handleMouseUp);
+    };
+
+    resizableBlock.addEventListener("mousedown", handleMouseDown);
+
+    return () => {
+      resizableBlock.removeEventListener("mousedown", handleMouseDown);
+      document.removeEventListener("mousemove", handleMouseMove);
+      document.removeEventListener("mouseup", handleMouseUp);
+    };
+  }, []);
+
   return (
     <div className={styles.page}>
       <Navbar />
       <div className={clsx(styles.body, { [styles.open]: activeTask !== -1 })}>
-        <Sidebar/>
+        <Sidebar />
         <MainPageContent />
-        <CasesBar />
+        <div className={styles.resizableBlock} id="resizableBlock">
+          <CasesBar />
+        </div>
       </div>
     </div>
   );
 };
+
+export { MainPage };
